3ae0c7a3a956300b2e31ec5e633a9dc3f6f5ab0a
Trenton Plager <tplager1@gmail.com>
2021-03-10 16:21:07 -0500

    Started some preliminary stuff for dialogue
    Still need to figure out how exactly we're managing what the player has so the dialogue manager can check

---
+++ CoolAir/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    #region Fields
+    private Dictionary<string, List<string>> dialogueLines = new Dictionary<string, List<string>>(); 
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ReadAllDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string RetrieveIdleDialogue(string characterName)
+    {
+        return dialogueLines[characterName][0]; 
+    }
+
+    public string RetreiveDialogueLine(string characterName, int lineNumber)
+    {
+        return dialogueLines[characterName][lineNumber]; 
+    }
+
+    private void ReadAllDialogue()
+    {
+        ReadDialogueLines("Doctor");
+        ReadDialogueLines("Mechanic");
+        ReadDialogueLines("OldLady");
+        ReadDialogueLines("Plumber");
+        ReadDialogueLines("ViceDeanLaybourne");
+    }
+
+    private void ReadDialogueLines(string characterName)
+    {
+        try
+        {
+            StreamReader input = new StreamReader("Assets/Dialogue/" + characterName + ".txt");
+
+            if (!dialogueLines.ContainsKey(characterName))
+            {
+                dialogueLines.Add(characterName, new List<string>());
+            }
+
+            while (!input.EndOfStream)
+            {
+                dialogueLines[characterName].Add(input.ReadLine());
+            }
+        }
+        catch
+        {
+            Debug.Log($"Problem reading in {characterName}'s dialogue"); 
+        }
+    }
+}
