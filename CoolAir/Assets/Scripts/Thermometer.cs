8e142bc2c9b56aa65d88abdc648753a8d328051e
John Vance <jmv7411@rit.edu>
2021-03-11 11:57:18 -0500

    Added Thermometer functionality

---
+++ CoolAir/Assets/Scripts/Thermometer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Thermometer : MonoBehaviour
+{
+    private Text thermometerText;
+    [SerializeField]
+    private int temp;
+    [SerializeField]
+    private float time;
+    private int count;
+
+
+    void Start()
+    {
+        time = 0;
+        count = 0;
+        temp = 56;
+        thermometerText = this.GetComponent<Text>();
+        thermometerText.text = "" + temp;
+    }
+
+    void Update()
+    {
+        if(count >= 20 && temp >= 71)
+        {
+            // End the game
+
+        }
+
+        else
+        {
+            time += Time.deltaTime;
+
+            if(time >= 15)
+            {
+                time = 0;
+                count++;
+                temp++;
+                thermometerText.text = "" + temp;
+
+            }
+
+        }
+
+
+
+    }
+}
