4843eaf26c25fa7f736c7742c49513ba3a048387
trimute2 <ben.j.stern@gmail.com>
2021-03-15 18:19:48 -0400

    refactored air conditioner

--- CoolAir/Assets/Scripts/AirConditionerScript.cs
+++ CoolAir/Assets/Scripts/AirConditionerScript.cs
@@ -5,8 +5,6 @@ using UnityEngine.UI;
 
 public class AirConditionerScript : MonoBehaviour
 {
-    public Inventory inventory;
-    public PlayerController player;
     private List<Item.ItemType> acInv;
     [SerializeField] private GameObject speechBubble;
     private Text dialogueText;
@@ -30,43 +28,43 @@ public class AirConditionerScript : MonoBehaviour
     {
         if (acInv.Count == 0)
         {
-            if(inventory.Contains(Item.ItemType.Wrench))
+            if(Inventory.Instance.Contains(Item.ItemType.Wrench))
                 AddItem(Item.ItemType.Wrench);
 
             else
             {
                 dialogueText.text = "It looks like some of these bolts are lose...";
 
-                if(!player.neededItems.Contains(Item.ItemType.Wrench))
-                    player.neededItems.Push(Item.ItemType.Wrench);
+                if(!PlayerController.Instance.neededItems.Contains(Item.ItemType.Wrench))
+					PlayerController.Instance.neededItems.Push(Item.ItemType.Wrench);
             }
         }
 
         if (acInv.Count == 1)
         {
-            if (inventory.Contains(Item.ItemType.Pump))
+            if (Inventory.Instance.Contains(Item.ItemType.Pump))
                 AddItem(Item.ItemType.Pump);
 
             else
             {
                 dialogueText.text = "The screws are fine but it is not pumping anymore...";
 
-                if (!player.neededItems.Contains(Item.ItemType.Pump))
-                    player.neededItems.Push(Item.ItemType.Pump);
+                if (!PlayerController.Instance.neededItems.Contains(Item.ItemType.Pump))
+					PlayerController.Instance.neededItems.Push(Item.ItemType.Pump);
             }
         }
 
         if (acInv.Count == 2)
         {
-            if (inventory.Contains(Item.ItemType.Ammonia))
+            if (Inventory.Instance.Contains(Item.ItemType.Ammonia))
                 AddItem(Item.ItemType.Ammonia);
 
             else
             {
                 dialogueText.text = "Now I just need to find some ammonia...";
 
-                if (!player.neededItems.Contains(Item.ItemType.Ammonia))
-                    player.neededItems.Push(Item.ItemType.Ammonia);
+                if (!PlayerController.Instance.neededItems.Contains(Item.ItemType.Ammonia))
+					PlayerController.Instance.neededItems.Push(Item.ItemType.Ammonia);
             }
         }
     }
@@ -74,6 +72,6 @@ public class AirConditionerScript : MonoBehaviour
     public void AddItem(Item.ItemType itemType)
     {
         acInv.Add(itemType);
-        player.RemoveItem(itemType);
+		PlayerController.Instance.RemoveItem(itemType);
     }
 }
